f105e0d58c6927f00bc05ce6ba5ad5da4bc64221
lucasgaertner <bartner@msn.com>
2021-01-23 23:32:53 +0100

    JS modified header navigation page

--- ChildrenCare/ChildrenCare/static/scripts/script.js
+++ ChildrenCare/ChildrenCare/static/scripts/script.js
@@ -4,6 +4,7 @@ function setHeader(){
     var currenturl = window.location.href;
     var header_split = currenturl.split('/');
     var last = header_split[header_split.length-2];
+    var login = header_split[header_split.length-4];
     for (i=0; i < location_arr.length; i++){
         if (location_arr[i] == last){
             var tmp_site = document.getElementById(location_arr[i]);
@@ -13,6 +14,12 @@ function setHeader(){
             var tmp_site = document.getElementById('home');
             tmp_site.classList.add('active');
         }
+        else if(login == "login"){
+            var tmp_site = document.getElementById('login');
+            tmp_site.classList.add('active');
+            var tmp_last = header_split[header_split.length-2];
+            changedHeader(tmp_last);
+        }
     }
 }
 
@@ -20,3 +27,4 @@ function changedHeader(link){
     var old_site = document.getElementById(link);
     old_site.classList.remove('active');
 }
+
