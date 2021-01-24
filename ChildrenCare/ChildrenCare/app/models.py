61d7f2353873f417ae5113c47c95e42fbc701ea3
lucasgaertner <bartner@msn.com>
2021-01-24 15:03:25 +0100

    Added Visualization

--- ChildrenCare/ChildrenCare/app/models.py
+++ ChildrenCare/ChildrenCare/app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Profile(models.Model):
     name = models.CharField(max_length=150)
-    nachname = models.EmailField(blank=True)
+    nachname = models.CharField(max_length=150)
     def __str__(self):
         return self.name
